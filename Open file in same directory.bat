87f04f7584ab4cec9f980d6ef0b075820fc21569
Blake-McCullough <66660752+Blake-McCullough@users.noreply.github.com>
2021-11-12 10:13:46 +0800

    Update Open file in same directory.bat

--- Open file in same directory.bat
+++ Open file in same directory.bat
@@ -1,7 +1,8 @@
-
+::Made By Blake McCullough
+::Discord - Spoiled_Kitten#4911
+::Github - https://github.com/Blake-McCullough/
+::Email - privblakemccullough@protonmail.com
 ::This opens file in same directory
 pushd %~dp0
 ProgramInSameFolderAsBat.exe
 popd
-::Made By Blake McCullough
-::https://github.com/Blake-McCullough
